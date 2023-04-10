5e3265823e233f1a98f86b38fcdefd68c2a5164b
Julieta359 <119441514+Julieta359@users.noreply.github.com>
2023-04-10 17:37:22 -0300

    Login, Experiencia y Educación

--- src/main/java/com/portfolio/jds/Controller/PersonaController.java
+++ src/main/java/com/portfolio/jds/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.jds.Entity.Persona;
 import com.portfolio.jds.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin (origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
 
     @Autowired
@@ -26,18 +27,21 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
 
+    @PreAuthorize("hasRole ('ADMIN')")
     @PostMapping("personas/crear")
     public String createPersona(@RequestBody Persona persona) {
         ipersonaService.savePersona(persona);
         return "La persona fue creada con éxito";
     }
 
+    @PreAuthorize("hasRole ('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id) {
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada correctamente";
     }
 
+    @PreAuthorize("hasRole ('ADMIN')")
     @PutMapping("personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
             @RequestParam("nombre") String nuevoNombre,
---
+++ src/main/java/com/portfolio/jds/Security/Repository/iRolRepository.java
@@ -0,0 +1,14 @@
+
+package com.portfolio.jds.Security.Repository;
+
+import com.portfolio.jds.Security.Entity.Rol;
+import com.portfolio.jds.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer>{
+    Optional <Rol> findByRolNombre (RolNombre rolNombre);
+}
